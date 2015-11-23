45379379fc883b30a3b406aa765d1ba48b97cd2b
Orion Letizi <oletizi@mac.com>
2015-11-22 20:00:05 -0800

    added a click on transient to the player

--- src/main/java/com/orion/sampler/tools/ui/Controller.java
+++ src/main/java/com/orion/sampler/tools/ui/Controller.java
@@ -23,6 +23,7 @@ import net.beadsproject.beads.core.AudioContext;
 import net.beadsproject.beads.core.AudioIO;
 import net.beadsproject.beads.core.Bead;
 import net.beadsproject.beads.core.UGen;
+import net.beadsproject.beads.core.io.JavaSoundAudioIO;
 import net.beadsproject.beads.core.io.NonrealtimeIO;
 import net.beadsproject.beads.data.Sample;
 import net.beadsproject.beads.ugens.BiquadFilter;
@@ -42,11 +43,12 @@ public class Controller implements EventHandler<Event>, ChangeListener<Number> {
   private final long frameCount;
   private final int channelCount;
   private final ScrollPane scrollPane;
+  private final Player player;
   private TransientLocator locator;
   private int samplesPerPixel = 1000;
   private int zoomFactor = 2;
   private int vertScale = 10000;
-  private float transientThreshold = 1.5f;
+  private float transientThreshold = 0.01f;
   private float transientZoomFactor = 1.5f;
 
   private ChangeListener<? super Number> widthListener = new ChangeListener<Number>() {
@@ -66,6 +68,7 @@ public class Controller implements EventHandler<Event>, ChangeListener<Number> {
     this.canvas = canvas;
     this.sampleFile = sampleFile;
     sample = new Sample(sampleFile.getAbsolutePath());
+    player = new Player(sample);
     locator = new TransientLocator(sample, transientThreshold);
     frameCount = sample.getNumFrames();
     channelCount = sample.getNumChannels();
@@ -95,7 +98,7 @@ public class Controller implements EventHandler<Event>, ChangeListener<Number> {
   }
 
   private void drawTransientMarkers() {
-
+    info("Drawing transient markers...");
     final List<Transient> transients = locator.getTransients();
     final double vCenter = canvas.getHeight() / 2;
     final GraphicsContext gc = canvas.getGraphicsContext2D();
@@ -105,6 +108,7 @@ public class Controller implements EventHandler<Event>, ChangeListener<Number> {
       double y = 0;
       double w = 1;
       double h = vCenter;
+      info("Marker at: " + x + ", y: " + y + ", w: " + w + ", h: " + h);
       gc.fillRect(x, y, w, h);
     }
 
@@ -159,6 +163,10 @@ public class Controller implements EventHandler<Event>, ChangeListener<Number> {
           increaseTransientThreshold();
         } else if (shouldDecreaseTransientThreshold()) {
           decreaseTransientThreshold();
+        } else if (shouldPlay()) {
+          player.play();
+        } else if (shouldStop()) {
+          player.pause();
         }
 
       } else if (keyEvent.getEventType().equals(KeyEvent.KEY_RELEASED)) {
@@ -169,6 +177,14 @@ public class Controller implements EventHandler<Event>, ChangeListener<Number> {
     } else {
       info("Event: " + event);
     }
+  }
+
+  private boolean shouldStop() {
+    return player.playing() && pressedKeys.size() == 1 && pressedKeys.contains(KeyCode.SPACE);
+  }
+
+  private boolean shouldPlay() {
+    return !player.playing() && pressedKeys.size() == 1 && pressedKeys.contains(KeyCode.SPACE);
   }
 
   private void decreaseTransientThreshold() {
@@ -230,6 +246,73 @@ public class Controller implements EventHandler<Event>, ChangeListener<Number> {
     return heightListener;
   }
 
+  private class Player extends Bead {
+    private final AudioContext ac;
+    private final Sample sample;
+    private final SamplePlayer player;
+    private final TransientLocator transientLocator;
+    private boolean playing = false;
+    private Sample click;
+
+    public Player(Sample sample) {
+      this(new AudioContext(new JavaSoundAudioIO()), sample);
+
+    }
+
+    public Player(AudioContext ac, Sample sample) {
+      super();
+      this.ac = ac;
+      this.sample = sample;
+      player = new SamplePlayer(ac, sample);
+      player.setEndListener(this);
+      ac.out.addInput(player);
+      transientLocator = new TransientLocator(ac, transientThreshold, this);
+      //ac.out.addDependent(player);
+      ac.start();
+      player.reset();
+      try {
+        click = new Sample(ClassLoader.getSystemResource("audio/click.wav").getFile());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    public void play() {
+      info("play!");
+      playing = true;
+      player.start();
+    }
+
+    public void playClick() {
+      final SamplePlayer clickPlayer = new SamplePlayer(ac, click);
+      ac.out.addInput(clickPlayer);
+      player.start();
+    }
+
+    public void pause() {
+      info("pause!");
+      playing = false;
+      player.pause(true);
+    }
+
+    public void reset() {
+      info("reset!");
+      player.reset();
+      pause();
+    }
+
+    @Override
+    protected void messageReceived(Bead message) {
+      super.messageReceived(message);
+      pause();
+      reset();
+    }
+
+    public boolean playing() {
+      return playing;
+    }
+  }
+
   private class Transient {
     double time;
     double sample;
@@ -242,24 +325,26 @@ public class Controller implements EventHandler<Event>, ChangeListener<Number> {
 
   private class TransientLocator extends Bead {
     private final AudioIO io = new NonrealtimeIO();
-    private final AudioContext ac = new AudioContext(io);
+    private final AudioContext ac;
+    private final float threshold;
     private final PeakDetector od;
-    private final SamplePlayer player;
-    private UGen out = ac.out;
-    private boolean running = true;
+    private final BiquadFilter filter;
     private final List<Transient> transients = new ArrayList<>();
+    private final Player clickPlayer;
+    private SamplePlayer player;
+    private UGen out;
+    private boolean running = true;
 
-    public TransientLocator(final Sample sample, float threshold) {
+    public TransientLocator(final AudioContext ac, float threshold, final Player clickPlayer) {
       super();
-      player = new SamplePlayer(ac, sample);
-      player.setEndListener(this);
+      this.ac = ac;
+      this.threshold = threshold;
+      this.clickPlayer = clickPlayer;
 
-      BiquadFilter filter = new BiquadFilter(ac, 2, BiquadFilter.Type.HP);
+      out = ac.out;
+
+      filter = new BiquadFilter(ac, 2, BiquadFilter.Type.HP);
       filter.setFrequency(4000f);
-      filter.addInput(player);
-      out.addInput(filter);
-      ac.out.addInput(out);
-
       /*
        * To analyse a signal, build an analysis chain.
        * We also manually set parameters of the sfs.
@@ -297,6 +382,18 @@ public class Controller implements EventHandler<Event>, ChangeListener<Number> {
 
       ac.out.addDependent(sfs);
 
+    }
+
+    public TransientLocator(final Sample sample, float threshold) {
+      //super();
+      this(new AudioContext(new NonrealtimeIO()), threshold, null);
+      player = new SamplePlayer(ac, sample);
+      player.setEndListener(this);
+      out.addDependent(player);
+
+      filter.addInput(player);
+      out.addInput(filter);
+      ac.out.addInput(out);
       //and begin
       ac.start();
     }
@@ -314,7 +411,10 @@ public class Controller implements EventHandler<Event>, ChangeListener<Number> {
         running = false;
       }
       if (message == od) {
-        //info("transient at: " + ac.getTime() + "ms, sample: " + ac.msToSamples(ac.getTime()));
+        info("transient at: " + ac.getTime() + "ms, sample: " + ac.msToSamples(ac.getTime()));
+        if (clickPlayer != null) {
+          clickPlayer.playClick();
+        }
         transients.add(new Transient(ac.getTime(), ac.msToSamples(ac.getTime())));
       }
     }
