f64db1a7b458b12cbc7bd11819e8f62c034cbea3
Orion Letizi <oletizi@mac.com>
2015-11-22 17:05:21 -0800

    basic wave drawing seems to work.

--- src/main/java/com/orion/sampler/tools/ui/Controller.java
+++ src/main/java/com/orion/sampler/tools/ui/Controller.java
@@ -1,4 +1,116 @@
 package com.orion.sampler.tools.ui;
 
-public class Controller {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.paint.Color;
+import net.beadsproject.beads.data.Sample;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class Controller implements EventHandler<Event>, ChangeListener<Number> {
+  private final Scene scene;
+  private final Group root;
+  private final Canvas canvas;
+  private final File sampleFile;
+  private final Sample sample;
+  private final long frameCount;
+  private final int channelCount;
+  private final ScrollPane scrollPane;
+  private int samplesPerPixel = 1000;
+  private int vertScale = 10000;
+  private ChangeListener<? super Number> widthListener = new ChangeListener<Number>() {
+    @Override
+    public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+      updateView();
+    }
+  };
+  private ChangeListener<? super Number> heightListener = widthListener;
+
+  public Controller(Scene scene, Group root, Canvas canvas, File sampleFile) throws IOException {
+    this.scene = scene;
+    this.root = root;
+    this.canvas = canvas;
+    this.sampleFile = sampleFile;
+    sample = new Sample(sampleFile.getAbsolutePath());
+    frameCount = sample.getNumFrames();
+    channelCount = sample.getNumChannels();
+    canvas.setWidth(frameCount / channelCount / samplesPerPixel);
+    scrollPane = new ScrollPane();
+    scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+    scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+    scrollPane.setContent(canvas);
+    scrollPane.setHmax(scene.getWidth());
+    root.getChildren().add(scrollPane);
+    drawWave();
+  }
+
+
+  public void updateView() {
+    scrollPane.setFitToHeight(true);
+    scrollPane.setPrefWidth(scene.getWidth());
+  }
+
+  private void drawWave() {
+    final GraphicsContext gc = canvas.getGraphicsContext2D();
+    final double vCenter = canvas.getHeight() / 2;
+    info("vCenter: " + vCenter);
+    gc.setFill(Color.BLACK);
+    gc.fillRect(0, vCenter, canvas.getWidth(), 1);
+
+    float[] buffer = new float[channelCount];
+    int x = 0;
+    float total = 0;
+    for (int i = 0; i < frameCount; i++) {
+      sample.getFrame(i, buffer);
+      double value = Math.abs(buffer[0]);
+      total += value;
+
+      if (i % samplesPerPixel == 0) {
+        double avg = total / samplesPerPixel;
+        ++x;
+        double h = avg * vertScale;
+        double y = vCenter - h;
+        double w = 1;
+        gc.fillRect(x, y, w, h);
+        total = 0;
+      }
+    }
+  }
+
+  private void info(String s) {
+    System.out.println(s);
+  }
+
+
+  @Override
+  public void handle(Event event) {
+    if ((event instanceof MouseEvent) || event instanceof ScrollEvent) {
+    } else {
+      info("Event: " + event);
+    }
+  }
+
+  @Override
+  public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+    info("observable: " + observable + ", oldValue: " + oldValue + ", newValue: " + newValue);
+  }
+
+  public ChangeListener<? super Number> getWidthListener() {
+    return widthListener;
+  }
+
+  public ChangeListener<? super Number> getHeightListener() {
+    return heightListener;
+  }
 }
--- src/main/java/com/orion/sampler/tools/ui/Main.java
+++ src/main/java/com/orion/sampler/tools/ui/Main.java
@@ -1,61 +1,37 @@
 package com.orion.sampler.tools.ui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.event.EventType;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.util.Random;
+import java.io.File;
+import java.net.URL;
 
 public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Parent parent = FXMLLoader.load(getClass().getResource("sample.fxml"));
-    final Group root = new Group(parent);
+    //Parent parent = FXMLLoader.load(getClass().getResource("com/orion/sampler/tools/ui/sample.fxml"));
+    final Group root = new Group();
     primaryStage.setTitle("Hello World");
     final Scene scene = new Scene(root, 700, 700);
-    primaryStage.setScene(scene);
 
     final Canvas canvas = new Canvas(700, 300);
-    final GraphicsContext gc = canvas.getGraphicsContext2D();
-    draw2DShapes(gc);
+    //root.getChildren().add(canvas);
 
-    root.getChildren().add(canvas);
+    primaryStage.setScene(scene);
+    final URL systemResource = ClassLoader.getSystemResource("audio/hh.wav");
+    final Controller controller = new Controller(scene, root, canvas, new File(systemResource.getFile()));
+
+    scene.addEventFilter(EventType.ROOT, controller);
+    scene.widthProperty().addListener(controller.getWidthListener());
+    scene.heightProperty().addListener(controller.getHeightListener());
 
     primaryStage.show();
-  }
-
-  private void draw2DShapes(GraphicsContext gc) {
-    double width = gc.getCanvas().getWidth();
-    double height = gc.getCanvas().getHeight();
-
-    Random random = new Random(System.currentTimeMillis());
-
-    gc.setFill(Color.rgb(random.nextInt(255), random.nextInt(255),
-        random.nextInt(255), 0.9));
-    gc.translate(width / 2, height / 2);
-
-    for (int i = 0; i < 60; i++) {
-      gc.rotate(6.0);
-      gc.setFill(Color.rgb(random.nextInt(255), random.nextInt(255),
-          random.nextInt(255), 0.9));
-      gc.fillOval(10, 60, 30, 30);
-      gc.strokeOval(60, 60, 30, 30);
-      gc.setFill(Color.rgb(random.nextInt(255), random.nextInt(255),
-          random.nextInt(255), 0.9));
-      gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-      gc.setFill(Color.rgb(random.nextInt(255), random.nextInt(255),
-          random.nextInt(255), 0.9));
-      gc.fillPolygon(
-          new double[]{105, 117, 159, 123, 133, 105, 77, 87, 51, 93},
-          new double[]{150, 186, 186, 204, 246, 222, 246, 204, 186, 186}, 10);
-    }
+    controller.updateView();
   }
 
   public static void main(String[] args) {
